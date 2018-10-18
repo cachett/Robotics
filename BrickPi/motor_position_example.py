7c86fed1a0ecec9db025df95a564bdfd530f33b2
Theo Cachet <tc5218@sprite01.doc.ic.ac.uk>
2018-10-18 12:57:16 +0100

    slight modifications and start of the graph script

--- BrickPi/motor_position_example.py
+++ BrickPi/motor_position_example.py
@@ -12,16 +12,18 @@ interface.motorEnable(motors[1])
 motorParams = interface.MotorAngleControllerParameters()
 motorParams.maxRotationAcceleration = 6.0
 motorParams.maxRotationSpeed = 12.0
-motorParams.feedForwardGain = 255/20.0
-motorParams.minPWM = 18.0
-motorParams.pidParameters.minOutput = -255
-motorParams.pidParameters.maxOutput = 255
-motorParams.pidParameters.k_p = 100.0
-motorParams.pidParameters.k_i = 0.0
-motorParams.pidParameters.k_d = 0.0
+motorParams.feedForwardGain = 255/20.0#To modify
+motorParams.minPWM = 18.0 #To modify
+motorParams.pidParameters.minOutput = -255#To modify
+motorParams.pidParameters.maxOutput = 255#To modify
+motorParams.pidParameters.k_p = 100.0#To modify
+motorParams.pidParameters.k_i = 100.0#To modify
+motorParams.pidParameters.k_d = 100.0#To modify
 
 interface.setMotorAngleControllerParameters(motors[0],motorParams)
 interface.setMotorAngleControllerParameters(motors[1],motorParams)
+
+interface.startLogging("logger.txt")
 
 while True:
 	angle = float(input("Enter a angle to rotate (in radians): "))
@@ -31,10 +33,11 @@ while True:
 	while not interface.motorAngleReferencesReached(motors) :
 		motorAngles = interface.getMotorAngles(motors)
 		if motorAngles :
-			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
+			print ("Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0])
 		time.sleep(0.1)
 
-	print "Destination reached!"
-	
+	print ("Destination reached!")
+
+interface.stopLogging()
 
 interface.terminate()
