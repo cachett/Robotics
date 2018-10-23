c27d2b85906f32ca7fab2ae6f676895ecae6c2d6
Clement Gain <cmg3818@texel41.doc.ic.ac.uk>
2018-10-23 10:13:45 +0100

    add graph creator

--- BrickPi/motor_position_example.py
+++ BrickPi/motor_position_example.py
@@ -10,8 +10,8 @@ interface.motorEnable(motors[0])
 interface.motorEnable(motors[1])
 
 motorParams = interface.MotorAngleControllerParameters()
-motorParams.maxRotationAcceleration = 6.0
-motorParams.maxRotationSpeed = 12.0
+motorParams.maxRotationAcceleration = 1.0 #6
+motorParams.maxRotationSpeed = 1.0 #12
 motorParams.feedForwardGain = 255/20.0
 motorParams.minPWM = 18.0
 motorParams.pidParameters.minOutput = -255
@@ -23,7 +23,9 @@ motorParams.pidParameters.k_d = 0.0
 interface.setMotorAngleControllerParameters(motors[0],motorParams)
 interface.setMotorAngleControllerParameters(motors[1],motorParams)
 
+
 while True:
+	interface.startLogging("logger.txt")
 	angle = float(input("Enter a angle to rotate (in radians): "))
 
 	interface.increaseMotorAngleReferences(motors,[angle,angle])
@@ -34,7 +36,9 @@ while True:
 			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
 		time.sleep(0.1)
 
-	print "Destination reached!"
-	
+	print("Destination reached!")
+
+interface.stopLogging()
+
 
 interface.terminate()
