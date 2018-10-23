967cca8604226c07ec28d63b604b0537d7e81662
Theo Cachet <tc5218@texel40.doc.ic.ac.uk>
2018-10-23 10:57:01 +0100

    turn method

--- BrickPi/myrobot.py
+++ BrickPi/myrobot.py
@@ -25,9 +25,9 @@ class MyRobot:
         self.interface.setMotorAngleControllerParameters(motors[0],self.motorParams)
         self.interface.setMotorAngleControllerParameters(motors[1],self.motorParams)
 
-    def specific_move(self, angle1, angle2):
+    def reach_target_angles(self, angle1, angle2):
         self.interface.increaseMotorAngleReferences(self.motors,[angle1,angle2])
-        previousdiff = 1000 #random value
+        previous_total_angle_difference = 1000 #random value
         count = 0
         while not self.interface.motorAngleReferencesReached(self.motors) :
             motorAngles = self.interface.getMotorAngles(self.motors)
@@ -35,33 +35,44 @@ class MyRobot:
             if motorAngles :
                 print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
                 print "Want to reach ", referenceAngles[0], ", ", referenceAngles[1]
-                if previousdiff == abs(motorAngles[0][0] - referenceAngles[0]):
+                total_angle_difference = abs(motorAngles[0][0] - referenceAngles[0]) + abs(motorAngles[1][0] - referenceAngles[1])
+                if previous_total_angle_difference == total_angle_difference:
                     count += 1
                 else:
                     count = 0
                 if count >= 10:
                     break
-                previousdiff = abs(motorAngles[0][0] - referenceAngles[0])
-                previousMotorAngles0 = motorAngles[0][0]
-                previousMotorAngles1 = motorAngles[1][0]
-            time.sleep(0.1)
+                previous_total_angle_difference = total_angle_difference
+            time.sleep(0.01)
 
-    def moveforward(self, angle):
-        self.specific_move(angle, angle)
+    def move_forward(self, angle):
+        self.reach_target_angles(angle, angle)
 
-    def moveforwardcm(self, distance):
-        self.moveforward((1.0/self.wheelradius)*distance)
+    def move_forward_cm(self, distance):
+        self.move_forward((1.0/self.wheelradius)*distance)
 
     def right90deg(self):
-        self.specific_move((self.robotwidth/(4.0*self.wheelradius))*math.pi, -(self.robotwidth/(4.0*self.wheelradius))*math.pi)
+        self.reach_target_angles((self.robotwidth/(4.0*self.wheelradius))*math.pi, -(self.robotwidth/(4.0*self.wheelradius))*math.pi)
+
+    def distance_to_wheel_angle(self, distance):
+        return distance/self.wheelradius
+
+
+    def turn(self, side, angle):
+        if side == "left":
+            self.reach_target_angles(-self.distance_to_wheel_angle(self, self.robotwidth*angle/2), self.distance_to_wheel_angle(self, self.robotwidth*angle/2) )
+        elif side == "right":
+            self.reach_target_angles(self.distance_to_wheel_angle(self, self.robotwidth*angle/2), -self.distance_to_wheel_angle(self, self.robotwidth*angle/2) )
+        else:
+            print("Unknown command. Expected 'left' or 'right' in turn method")
 
 
 robot = MyRobot([0,1], 3.5, 17.8)
-robot.moveforwardcm(40.0)
+robot.move_forward_cm(40.0)
 robot.right90deg()
-robot.moveforwardcm(40.0)
+robot.move_forward_cm(40.0)
 robot.right90deg()
-robot.moveforwardcm(40.0)
-robot.right90deg()
-robot.moveforwardcm(40.0)
+robot.move_forward_cm(40.0)
+robot.turn('right', math.pi/2)
+robot.move_forward_cm(40.0)
 robot.right90deg()
